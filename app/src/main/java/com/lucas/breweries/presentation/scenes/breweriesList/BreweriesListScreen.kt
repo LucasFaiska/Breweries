ea3aaec88a48de759e8591adaeda33564b07dffe
lucas <lucas.faiskaa@gmail.com>
2024-06-23 16:34:12 +0100

    showing breweries with style

--- app/src/main/java/com/lucas/breweries/presentation/scenes/breweriesList/BreweriesListScreen.kt
+++ app/src/main/java/com/lucas/breweries/presentation/scenes/breweriesList/BreweriesListScreen.kt
@@ -1,19 +1,26 @@
 package com.lucas.breweries.presentation.scenes.breweriesList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -22,6 +29,7 @@ import com.lucas.breweries.domain.model.Brewery
 import com.lucas.breweries.presentation.components.ErrorScreen
 import com.lucas.breweries.presentation.components.LoadingScreen
 import com.lucas.breweries.presentation.theme.FireBrick
+import com.lucas.breweries.presentation.theme.Onyx
 import com.lucas.breweries.presentation.theme.Timberwolf
 import com.lucas.breweries.presentation.theme.Typography
 
@@ -69,8 +77,7 @@ private fun BreweriesListScreenTitle(breweries: LazyPagingItems<Brewery>) {
         style = Typography.h1,
         color = Timberwolf,
         text = stringResource(
-            id = R.string.breweries_list_title,
-            breweries.itemCount
+            id = R.string.breweries_list_title, breweries.itemCount
         )
     )
 }
@@ -90,14 +97,39 @@ private fun BreweriesList(breweries: LazyPagingItems<Brewery>) {
 
 @Composable
 private fun BreweryItem(brewery: Brewery) {
-    Column(
+    Box(
         modifier = Modifier
-            .padding(dimensionResource(id = R.dimen.default_padding))
+            .background(color = Onyx)
+            .fillMaxWidth()
             .testTag(breweryItemTestTag)
+            .clickable {
+
+            }
     ) {
-        Text(text = brewery.name)
-        Text(text = brewery.city)
-        Text(text = brewery.state)
+
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.default_padding)),
+        ) {
+
+            Text(
+                style = Typography.body1,
+                fontWeight = FontWeight.Bold,
+                text = brewery.name,
+                color = Timberwolf
+            )
+
+            Text(
+                style = Typography.body1,
+                text = brewery.address,
+                color = Timberwolf
+            )
+
+            Text(text = "${brewery.city}, ${brewery.state}", color = Timberwolf)
+
+            Text(text = brewery.country, color = Timberwolf)
+        }
+
+        Divider(thickness = 1.dp, color = Black)
     }
 }
 
@@ -108,8 +140,7 @@ private fun ColumnScope.BottomLoadingIndicator() {
             .testTag(bottomLoadingIndicatorTestTag)
             .size(70.dp)
             .padding(dimensionResource(id = R.dimen.default_padding))
-            .align(Alignment.CenterHorizontally),
-        color = FireBrick
+            .align(Alignment.CenterHorizontally), color = FireBrick
     )
 }
 
